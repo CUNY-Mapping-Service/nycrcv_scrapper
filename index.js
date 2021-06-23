7ea76f5d55b359fb0cabb85741de96bb46bd897f
wfield-CUNY <wfield@gc.cuny.edu>
2021-06-22 21:16:08 -0400

    id formattin

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const rp = require('request-promise');
 const cheerio = require('cheerio');
-const fs = require('fs')
+const fs = require('fs');
+const { start } = require('repl');
 
 const startAD = 23;
 const endAD = 87;
 const baseurl = "https://web.enrboenyc.us/CD24306AD"
 const urlSuffix = "0.html";
 
-let csv = "ED,Reported";
+let csv = "FullID,AD,ED,Reported";
+
+let _date = new Date();
 
 main();
 
@@ -31,12 +34,12 @@ async function main() {
 				const csvStr = eds[ed].children[0]
 					.data
 					.replace(' ', '')
-					.replace('ED  ', `AD${i}-ED`)
-					.replace('ED   ', `AD${i}-ED`)
-					.replace('ED 1', `AD${i}-ED1`)
-					.replace('ED1,', 'ED 1,')
-					.replace('Total', `AD${i}-Total`)
-					.replace(`AD${i}-AD${i}`,`AD${i}`);
+					.replace('ED  ', `fid,${i},`)
+					.replace('ED   ', `fid,${i},`)
+					// .replace('ED 1', `AD${i}-ED1`)
+					// .replace('ED1,', 'ED 1,')
+					// .replace('Total', `AD${i}-Total`)
+					// .replace(`AD${i}-AD${i}`,`AD${i}`);
 
 				if(!csvStr.includes('(') && !csvStr.includes('Reported')){
 					csv += csvStr;
@@ -48,13 +51,20 @@ async function main() {
 
 	}
 	const csvArr = csv.split('\n');
+	
 	let filteredArr = csvArr.filter(row => !row.includes('Total'))
-				
-	writeToCSVFile(filteredArr.join('\n'))
+	let mappedArr = filteredArr.map((row) =>{
+		let splitArray = row.split(',');
+		splitArray[2] = splitArray[2].replace(' ','');
+		splitArray[0]=(splitArray[1]+(splitArray[2].padStart(3, '0'))).replace(' ','');
+		let joinArray = splitArray.join(',');
+		return joinArray
+	});
+	writeToCSVFile(mappedArr.join('\n'))
 }
 
 function writeToCSVFile(data) {
-	const filename = 'output.csv';
+	const filename = `${_date.getMonth()+1}${_date.getDate()}${_date.getHours()}${_date.getMinutes()}-output.csv`;
 	fs.writeFile(filename, data, err => {
 		if (err) {
 			console.log('Error writing to csv file', err);

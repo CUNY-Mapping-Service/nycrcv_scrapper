916e51c716e30ffa01b5f901ee40e195796ed160
wfield-CUNY <wfield@gc.cuny.edu>
2021-11-09 21:39:59 -0500

    enter race

--- index.js
+++ index.js
@@ -5,18 +5,66 @@ const {
 	start
 } = require('repl');
 
-const startAD = 3;
-const endAD = 87;
-const baseurl = "https://web.enrboenyc.us/CD24306AD"
-const urlSuffix = "0.html";
-
-let csv = "FullID,AD,ED,Reported";
+const readline = require("readline");
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
 let _date = new Date();
 
-main();
+let race_base = {
+	mayor:'CD23376AD',
+	public_advocate:'CD23370AD',
+	city_comptroller:'CD23385AD',
+	borough_president_ny:'CD23365AD',
+	borough_president_bronx:'CD23366AD',
+	borough_president_kings:'CD23367AD',
+	borough_president_queens:'CD23368AD',
+	borough_president_richmond:'CD23369AD',
+	prop_1:'CD24566AD',
+	prop_2:'CD24567AD',
+	prop_3:'CD24568AD',
+	prop_4:'CD24569AD',
+	prop_5:'CD24570AD',
+	"cd_[1-51]":''
+}
 
-async function main() {
+rl.question("Enter race or type l to list all options:", function(cdNum) {
+	if(cdNum === 'l'){
+		console.log('#########################')
+		console.log()
+		console.log('OPTIONS:');
+		Object.keys(race_base).forEach((key)=>{
+			console.log('    '+key)
+		});
+		console.log()
+		console.log('#########################')
+		process.exit(0);
+	}
+	if((!race_base[cdNum] || typeof race_base[cdNum] === 'undefined') && !cdNum.includes('cd_')){
+		console.error('ERROR: You did not enter a valid race. Try again or type l to list all options.');
+		process.exit(0);
+	}
+	//
+	const raceBase = cdNum.includes('cd_')? `CD23${390+cdNum}AD` : race_base[cdNum];
+	const baseurl = `https://web.enrboenyc.us/${raceBase}`
+	main(baseurl);
+});
+
+
+
+
+
+
+async function main(baseurl) {
+	const startAD = 23;
+	const endAD = 87;
+
+	const urlSuffix = "0.html";
+
+	let csv = "FullID,AD,ED,Reported";
+
 	const numDistricts = endAD - startAD;
 	for (let i = startAD; i < endAD + 1; i++) {
 		console.log(`Loading Assembly District ${i}: ${((i-startAD)/numDistricts)*100}% complete`);
@@ -78,5 +126,6 @@ function writeToCSVFile(data) {
 		} else {
 			console.log(`saved as ${filename}`);
 		}
+		process.exit(0);
 	});
 }
